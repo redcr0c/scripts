38f0ce1923f2a2536e1679a9c4739f287f90647b
redcr0c <redcr0c.board@gmail.com>
2025-10-27 08:33:54 +0100

    Added number guessing gaming

---
+++ number_guessing.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int t;  
+    cin >> t;
+    
+    for (int i = 0; i < t; i++) {
+        int a,b;
+        cin >> a >> b;
+        
+        int n;  // Maximum number of guesses
+        cin >> n;
+        
+        int low = a + 1;  // +1 because A is inclusive
+        int high = b;
+        
+        while (true) {
+            // Binary search
+            int mid = (low + high) / 2;
+            cout << mid << endl;
+            cout.flush();
+            
+            string response;
+            cin >> response;
+            
+            if (response == "CORRECT") {
+                break;  // We found the number!
+            } else if (response == "TOO_SMALL") {
+                low = mid + 1;  // Number is larger than mid
+            } else if (response == "TOO_BIG") {
+                high = mid - 1;  // Number is smaller than mid
+            } else if (response == "WRONG_ANSWER") {
+                return 1;
+            }
+        }
+    }
+    
+    return 0;
+}
